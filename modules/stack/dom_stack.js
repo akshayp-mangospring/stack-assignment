1e63ff427bd416eb2f692bf9f1b6d3a4c0b9d61e
akshayp-mangospring <akshayp@mangospring.com>
2022-04-24 13:01:28 +0530

    Add support for pushing to stack on Enter keypress

--- modules/stack/dom_stack.js
+++ modules/stack/dom_stack.js
@@ -1,11 +1,29 @@
 import IntStack from './int_stack';
 
 function DomStack() {
+  const parser = new DOMParser();
+  const inputElem = document.getElementById('stack-input');
+  const pushButton = document.getElementById('push-button');
+  const stackElem = document.getElementById('stack');
+
+  function pushItemCallback() {
+    const valueToInsert = inputElem.value;
+    if (!valueToInsert) {
+      pushButton.setAttribute('disabled', 'disabled');
+      return;
+    }
+
+    if (intStack.push(parseInt(valueToInsert, 10))) {
+      const hue = Math.floor(Math.random() * 360) + 1;
+      const saturation = Math.floor(Math.random() * 250) + 1;
+      const htmlStr = `<li class="stack-item" style="background-color: hsl(${hue}deg ${saturation}% 89%);">${valueToInsert}</li>;`
+      const newStackElem = parser.parseFromString(htmlStr, 'text/html').body.firstChild;
+      stackElem.appendChild(newStackElem);
+      inputElem.value = '';
+    }
+  }
+
   function domContentLoaded(e) {
-    const inputElem = document.getElementById('stack-input');
-    const pushButton = document.getElementById('push-button');
-    const stackElem = document.getElementById('stack');
-    const parser = new DOMParser();
     const intStack = new IntStack();
     window.intStack = intStack;
 
@@ -21,22 +39,13 @@ function DomStack() {
       }
     });
 
-    pushButton.addEventListener('click', () => {
-      const valueToInsert = inputElem.value;
-      if (!valueToInsert) {
-        pushButton.setAttribute('disabled', 'disabled');
-        return;
-      }
-
-      if (intStack.push(parseInt(valueToInsert, 10))) {
-        const hue = Math.floor(Math.random() * 360) + 1;
-        const saturation = Math.floor(Math.random() * 250) + 1;
-        const htmlStr = `<li class="stack-item" style="background-color: hsl(${hue}deg ${saturation}% 89%);">${valueToInsert}</li>;`
-        const newStackElem = parser.parseFromString(htmlStr, 'text/html').body.firstChild;
-        stackElem.appendChild(newStackElem);
-        inputElem.value = '';
+    inputElem.addEventListener('keypress', (e) => {
+      if(e.key.toLowerCase() === 'enter') {
+        pushItemCallback();
       }
     });
+
+    pushButton.addEventListener('click', pushItemCallback);
 
     stackElem.addEventListener('click', (e) => {
       const clickedElem = e.target;
